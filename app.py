463ad33d64b04034f3ac0f364a6b1247969bb1e5
Paipoi8387 <ats07288387@outlook.jp>
2021-01-06 02:05:48 +0900

    version try

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-
 from flask import Flask, request, abort
 import os
 
 from linebot import (
-            LineBotApi, WebhookHandler
-            )
+    LineBotApi, WebhookHandler
+)
 from linebot.exceptions import (
-            InvalidSignatureError
-            )
+    InvalidSignatureError
+)
 from linebot.models import (
-            MessageEvent, TextMessage, TextSendMessage,
-            )
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
@@ -23,32 +22,32 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello_world():
-        return "hello world!"
+    return "hello world!"
 
-    @app.route("/callback", methods=['POST'])
-    def callback():
-            # get X-Line-Signature header value
-                signature = request.headers['X-Line-Signature']
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-                    # get request body as text
-                        body = request.get_data(as_text=True)
-                            app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-                                # handle webhook body
-                                    try:
-                                                handler.handle(body, signature)
-                                                    except InvalidSignatureError:
-                                                                abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-                                                                    return 'OK'
+    return 'OK'
 
-                                                                @handler.add(MessageEvent, message=TextMessage)
-                                                                def handle_message(event):
-                                                                        line_bot_api.reply_message(
-                                                                                        event.reply_token,
-                                                                                                TextSendMessage(text=event.message.text))
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
-                                                                        if __name__ == "__main__":
-                                                                            #    app.run()
-                                                                                port = int(os.getenv("PORT"))
-                                                                                    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
